95d82d361e56b381ea968cf8acca04ba050c2dde
vitiya99 <virajlakshithabandara@gmail.com>
2020-12-08 14:54:53 +0530

    Update the Project
    Add two file as Alert.js and List.js

---
+++ src/app.component/List.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const List = ({ items, removeItem, editItem }) => {
+  return (
+    <div className='task-list'>
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article className='task-item' key={id}>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button
+                type='button'
+                className='edit-btn'
+                onClick={() => editItem(id)}
+              >
+                Edit
+              </button>
+              <button
+                type='button'
+                className='delete-btn'
+                onClick={() => removeItem(id)}
+              >
+                Delete
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
